22f43f455ecc3880e8a6d913fbc12e1cda44c44d
David Diaz <david.diaz@e.inc>
2021-04-08 14:39:13 -0400

    fix dynamo

--- dynamo.tf
+++ dynamo.tf
@@ -1,6 +1,5 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "GameScores"
-  region 	 = "us-east-1"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
